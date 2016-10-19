efbe1e463fa7a95c3a41c50da38f36ede90259d1
pellepersson <hej@pellepersson.se>
2016-10-19 12:00:20 +0200

    hack to make sure background circle is showing

--- src/CircularProgress.js
+++ src/CircularProgress.js
@@ -39,7 +39,7 @@ export default class CircularProgress extends React.Component {
 
   render() {
     const { size, width, tintColor, backgroundColor, rotation, style, children } = this.props;
-    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, 360);
+    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, 360 * 0.999999999);
 
     const fill = this.extractFill((this.props.fill === 100) ? 99.999 : this.props.fill);
     const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, 360 * fill / 100);
